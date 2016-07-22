b5ec61c4b755d15859da4a00fd445c5e7ea7dbad
Joao Souza <d3estudio@gmail.com>
2016-07-22 18:19:34 -0300

    Create static folders

---
+++ app/public/css/style.css
@@ -0,0 +1,11 @@
+var socket = io();
+
+socket.on('connected', function(socket) {
+  console.log('connected');
+});
+
+socket.on('message', function(msg){
+  console.log(msg);
+
+  $('#name').text(msg);
+});
