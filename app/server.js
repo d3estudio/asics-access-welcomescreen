a24de94619a1cb5ae1ff4da7c1862a5ed10ced4b
Joao Souza <d3estudio@gmail.com>
2016-07-22 17:33:29 -0300

    Initial

--- app/server.js
+++ app/server.js
@@ -1,15 +1,41 @@
 'use strict';
 
-const express = require('express');
-
-// Constants
 const PORT = 8080;
+const express = require('express');
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var url = require('url');
 
-// App
-const app = express();
-app.get('/', function (req, res) {
-  res.send('Hello world\n');
+
+app.use(express.static('public'));
+
+io.on('connection', function(socket){
+  console.log('Socket connected!');
+  socket.emit('connected', {});
+
+	socket.on('message', function (msg) {
+		console.log('message: ', msg);
+		socket.emit('message', msg);
+	});
 });
 
-app.listen(PORT);
-console.log('Running on http://localhost:' + PORT);
+app.get('/', function(req, res){
+  res.sendfile('index.html');
+});
+
+app.get('/post', function(req, res){
+  var queryData = url.parse(req.url, true).query;
+  console.log(queryData.name);
+
+  io.emit('message', queryData.name);
+
+  io.sockets.emit(queryData.name);
+
+  res.sendStatus(200);
+});
+
+http.listen(PORT, function(){
+  console.log('listening on *:49160');
+});
+
