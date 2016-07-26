561ef675700a67de7c7d41307f95ff01203dc92c
phillfer <philliperamos@gmail.com>
2016-07-26 16:22:12 -0300

    added name appearing on top of the video when called

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -1,11 +1,26 @@
 var socket = io();
 
+$(document).ready(function () {
+  $('#content').hide();
+});
+
 socket.on('connected', function(socket) {
   console.log('connected');
 });
 
 socket.on('message', function(msg){
-  console.log(msg);
+  var content = $('#content');
+  var name = $('#name');
+  var video = $('#video');
 
-  $('#name').text(msg);
+  video.get(0).pause();
+  video.get(0).currentTime = '0';
+  content.show();
+  name.text(msg);
+
+  setTimeout(function () {
+    video.get(0).play();
+    content.hide();
+    name.text('');
+  }, 5000)
 });
