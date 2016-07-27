dea8286d29e133b934ffb3a400ba444afbe3e40c
phillfer <philliperamos@gmail.com>
2016-07-26 21:45:58 -0300

    added ease out animation for content

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -5,43 +5,68 @@ socket.on('connected', function(socket) {
   console.log('connected');
 });
 
-socket.on('message', function(msg){
+socket.on('message', function(guestName){
   var content = $('#content');
   var name = $('#name');
   var welcome = $('#welcome-message');
   var video = $('#video');
 
-  clearTimeout(nameTimeout);
-
-  $('#name-container').textfill({
-    debug: true,
-    maxFontPixels: 1580
-  });
-
   video.get(0).pause();
   video.get(0).currentTime = '0';
 
-  content.removeClass('hide');
-  content.addClass('show');
+  clearTimeout(nameTimeout);
 
-  name.text(msg);
-  name.removeClass('hide-name');
-  name.addClass('show-name');
+  resizeText();
 
-  welcome.removeClass('hide-welcome');
-  welcome.addClass('show-welcome');
+  showNameContainer(guestName);
 
   nameTimeout = setTimeout(function () {
     name.removeClass('show-name');
     name.addClass('hide-name');
-    // name.text('');
 
     welcome.addClass('hide-welcome');
     welcome.removeClass('show-welcome');
 
     video.get(0).play();
 
-    // content.removeClass('show');
-    // content.addClass('hide');
+    hideContent();
+
   }, 5000)
+
 });
+
+function resizeText() {
+  $('#name-container').textfill({
+    debug: true,
+    maxFontPixels: 1580
+  });
+}
+
+function showNameContainer(guestName) {
+  var content = $('#content');
+  var name = $('#name');
+  var welcome = $('#welcome-message');
+  var video = $('#video');
+
+  content.removeClass('hide');
+  content.addClass('show');
+
+  name.text(guestName);
+  name.removeClass('hide-name');
+  name.addClass('show-name');
+
+  welcome.removeClass('hide-welcome');
+  welcome.addClass('show-welcome');
+}
+
+function hideContent() {
+  var content = $('#content');
+  var name = $('#name');
+
+  setTimeout(function () {
+    name.text('');
+    content.removeClass('show');
+    content.addClass('hide');
+  }, 300)
+
+}
