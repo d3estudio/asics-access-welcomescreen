d46f79416615e5c470b2f3a67b6f82bc8b160ac5
phillfer <philliperamos@gmail.com>
2016-07-26 21:15:49 -0300

    added fold and unfold animations for welcome message

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -1,4 +1,5 @@
 var socket = io();
+var nameTimeout;
 
 socket.on('connected', function(socket) {
   console.log('connected');
@@ -7,7 +8,10 @@ socket.on('connected', function(socket) {
 socket.on('message', function(msg){
   var content = $('#content');
   var name = $('#name');
+  var welcome = $('#welcome-message');
   var video = $('#video');
+
+  clearTimeout(nameTimeout);
 
   $('#name-container').textfill({
     debug: true,
@@ -16,18 +20,28 @@ socket.on('message', function(msg){
 
   video.get(0).pause();
   video.get(0).currentTime = '0';
+
   content.removeClass('hide');
   content.addClass('show');
+
   name.text(msg);
   name.removeClass('hide-name');
   name.addClass('show-name');
 
-  setTimeout(function () {
+  welcome.removeClass('hide-welcome');
+  welcome.addClass('show-welcome');
+
+  nameTimeout = setTimeout(function () {
     name.removeClass('show-name');
     name.addClass('hide-name');
+    // name.text('');
+
+    welcome.addClass('hide-welcome');
+    welcome.removeClass('show-welcome');
+
     video.get(0).play();
-    content.removeClass('show');
-    content.addClass('hide');
-    name.text('');
+
+    // content.removeClass('show');
+    // content.addClass('hide');
   }, 5000)
 });
